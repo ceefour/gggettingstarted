65224ec722d3e1ab8eb133aa072f411f9dd05b62
Hendy Irawan <ceefour666@gmail.com>
2014-07-04 20:10:57 -0430

    object gone after stop

--- src/main/java/com/hendyirawan/gggettingstarted/simple/BasicOperations2.java
+++ src/main/java/com/hendyirawan/gggettingstarted/simple/BasicOperations2.java
@@ -7,20 +7,38 @@ import org.gridgain.grid.cache.GridCache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BasicOperations {
+public class BasicOperations2 {
 
 	private static final Logger log = LoggerFactory
-			.getLogger(BasicOperations.class);
+			.getLogger(BasicOperations2.class);
+	
+	public static class Oya {
+		String name;
+
+		public Oya(String name) {
+			super();
+			this.name = name;
+		}
+
+		/* (non-Javadoc)
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			return "Oya [name=" + name + "]";
+		}
+		
+	}
 	
 	public static void main(String[] args) throws GridException {
 		try (Grid grid = GridGain.start(
 				System.getProperty("user.home") + "/gridgain-platform-os-6.1.9-nix/examples/config/example-cache.xml")) {
-			GridCache<Integer, String> cache = grid.cache("partitioned");
+			GridCache<Integer, Oya> cache = grid.cache("partitioned");
 //			String oldVal = cache.put(1, "1");
 //			boolean success = cache.putx(2, "2");
-//			boolean success2 = cache.putxIfAbsent(3, "3");
-//			log.info("Current 3 value = {}", cache.get(3));
-//			cache.transform(3, (it) -> it + "-transformed");
+			boolean success2 = cache.putxIfAbsent(3, new Oya("3"));
+			log.info("Current 3 value = {}", cache.get(3));
+			cache.transform(3, (it) -> new Oya(it.name + "-transformed"));
 			log.info("Transformed 3 value = {}", cache.get(3));
 		}
 	}
