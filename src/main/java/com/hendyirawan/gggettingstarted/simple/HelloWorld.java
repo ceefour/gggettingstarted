c42b8aad8eaa7c4131ba5cfb1b13d8636b4a4509
Hendy Irawan <ceefour666@gmail.com>
2014-07-05 08:33:08 -0430

    fix according to @dsetrakyan's https://github.com/gridgain/gridgain/issues/74#issuecomment-48077212

--- src/main/java/com/hendyirawan/gggettingstarted/simple/HelloWorld.java
+++ src/main/java/com/hendyirawan/gggettingstarted/simple/HelloWorld.java
@@ -3,13 +3,15 @@ package com.hendyirawan.gggettingstarted.simple;
 import org.gridgain.grid.Grid;
 import org.gridgain.grid.GridException;
 import org.gridgain.grid.GridGain;
+import org.gridgain.grid.lang.GridRunnable;
 
 public class HelloWorld {
 
 	public static void main(String[] args) throws GridException {
 		try (Grid grid = GridGain.start(
 				System.getProperty("user.home") + "/gridgain-platform-os-6.1.9-nix/examples/config/example-compute.xml")) {
-			grid.compute().broadcast( () -> System.out.println("Hello") ).get();
+			grid.forRemotes().compute().run( (GridRunnable) () -> System.out.println("Hello remote!") ).get();
+			grid.compute().broadcast( (GridRunnable) () -> System.out.println("Hello everyone!") ).get();
 		}
 	}
 
